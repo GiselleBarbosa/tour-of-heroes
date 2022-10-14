b6870e06e09eb15985946beeb153c84b93025c7d
GiselleBarbosa <g88barbosa@gmail.com>
2022-10-13 22:52:49 -0300

    lista heroes

---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero.model"
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Wolverine' },
+  { id: 2, name: 'Huck' },
+  { id: 3, name: 'Batman' },
+  { id: 4, name: 'Superman' },
+  { id: 5, name: 'Mulher Maravilha' },
+  { id: 6, name: 'Spider Man' },
+  { id: 7, name: 'Flash' },
+  { id: 8, name: 'Viuva Negra' },
+  { id: 9, name: 'Homem de Ferro' },
+  { id: 10, name: 'Capitao America' },
+]
